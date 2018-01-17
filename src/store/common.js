6553ca199db6688999625ae3bed292159674577f
Mr.mshao <mshao_love@126.com>
2018-01-17 15:59:59 +0800

    公共模块

--- src/store/common.js
+++ src/store/common.js
@@ -1,27 +1,41 @@
+import { Toast } from 'antd-mobile'
+
 // actions
 export const types  = {
-  SHOWLOADING: 'common/SHOWLOADING',
-  HIDELOADING: 'common/HIDELOADING'
+  REQUEST_START: 'common/SHOWLOADING',
+  REQUEST_SUCCESS: 'common/HIDELOADING',
+  REQUEST_FAIL: 'common/SHOWERROR'
 }
 
 // init
 const initialState = {
-  isLoading: false
+  isLoading: false,
+  errInfo: {
+    status: false,
+    msg: ''
+  }
 }
 
 // reducer
-export default function reducer (state = initialState, actions = {}) {
-  switch (actions.type) {
-    case types.SHOWLOADING:
+export default function reducer (state = initialState, action) {
+  switch (action.type) {
+    case types.REQUEST_START: // 开始请求
       return { ...state, isLoading: true }
-    case types.HIDELOADING:
+
+    case types.REQUEST_SUCCESS: // 请求成功
       return { ...state, isLoading: false }
+
+    case types.REQUEST_FAIL: // 请求失败，返回失败提示信息
+      Toast.fail('加载数据错误...');
+      return { ...state, isLoading: false, errInfo: action.errInfo }
+
     default: return state
   }
 }
 
 // action creators 
 export const actions = {
-  showLoading: () => ({type: types.SHOWLOADING}),
-  hideLoading: () => ({type: types.HIDELOADING})
+  requestStart: () => ({type: types.REQUEST_START}),
+  requestSuccess: () => ({type: types.REQUEST_SUCCESS}),
+  requestFail: (err) => ({type: types.REQUEST_FAIL, errInfo: {status: true, msg: err}})
 }
