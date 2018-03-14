750616503876b2411c0d2b730d6afe098221ae25
Mr.mshao <mshao_love@126.com>
2018-03-14 21:48:29 +0800

    删除V平台样式，对iOS导航条处理，Android则不需处理，使用antd的样式

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './router';
-import './assets/common.less'
+// import './assets/common.less'
 import configureStore from './store/index'
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
