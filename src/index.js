acea3159d4c213d4f7d9b249b99732c0def6e393
Mr.mshao <mshao_love@126.com>
2018-04-02 16:43:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Router';
 import './utils/userInfo'
-import 'antd-mobile/dist/antd-mobile.css'; 
 import './assets/common.less'
 import registerServiceWorker from './registerServiceWorker'
 import config from './utils/config'
