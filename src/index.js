da9a938d8befbad8715ee5c222af33ea0dc87733
Mr.mshao <mshao_love@126.com>
2018-04-02 15:30:59 +0800

    引入antd

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Router';
 import './utils/userInfo'
+import 'antd-mobile/dist/antd-mobile.css'; 
 import './assets/common.less'
 import registerServiceWorker from './registerServiceWorker'
 import config from './utils/config'
